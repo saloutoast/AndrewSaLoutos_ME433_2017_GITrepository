8a797a283e0c3d4a6810343b0078ee0c7cb633c5
Andrew SaLoutos <andrewsaloutos2018@u.northwestern.edu>
2017-04-12 00:08:32 -0500

    Working on expander functions

--- HW5/HW5.X/expander.c
+++ HW5/HW5.X/expander.c
@@ -6,13 +6,20 @@
 #include "expander.h"
 
 void initExpander(void) { // initializes the I/O expander
-
+    
+    ANSELBbits.ANSB2 = 0; // turn off analog functionality so pins will work with I2C2
+    ANSELBbits.ANSB3 = 0;
+    
+    I2C2_master_setup();
+    
+    // define slave address? initialize registers and pull up resistors
+    
 }
 
 void setExpander(char pin, char level) { // sets values of pins on I/O expander
-
+    ; 
 }
 
 char getExpander(void) { // returns values of pins on I/O expander
-
+    ;
 }
