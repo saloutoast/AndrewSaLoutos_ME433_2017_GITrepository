154a43faa7e6278a6e1665f291b3f83a79d26f0d
Andrew SaLoutos <andrewsaloutos2018@u.northwestern.edu>
2017-06-06 13:15:10 -0500

    lower base speed, only far pixels

--- FinalProject/app/src/main/java/com/example/andrew/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/andrew/finalproject/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
             int far_pix = 0;
             int com_near = 0;
             int near_pix = 0;
-            for (int j = 0; j < bmp.getHeight(); j+=5) {// index through every five rows
+            for (int j = 0; j < bmp.getHeight()/2; j+=5) {// index through every five rows
                 int[] pixels = new int[bmp.getWidth()]; // pixels[] is the RGBA data
                 int startY = j; // which row in the bitmap to analyze to read
                 bmp.getPixels(pixels, 0, bmp.getWidth(), 0, startY, bmp.getWidth(), 1);
@@ -217,13 +217,13 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
             canvas.drawCircle(com_near, 3*(bmp.getHeight()/4), 10, paint1);
 
             // Send motor control based on two com values
-            int base_spd = 40;
-            int err_bound_near = 25;
+            int base_spd = 30;
+            int err_bound_near = 2000;
             int err_bound_far = 5;
             int err_near = com_near - (bmp.getWidth()/2);
             int err_far = com_far - (bmp.getWidth()/2);
-            double gain_near = 1.5;
-            double gain_far = 0.5;
+            double gain_near = 0.2;
+            double gain_far = 0.4;
             double correction_near = abs(err_near*gain_near);
             double correction_far = abs(err_far*gain_far);
 
